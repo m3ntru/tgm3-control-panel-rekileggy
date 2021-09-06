37fffa3ab36dd0e8a6040d70acb92c729647c6c6
m3ntru <zatd39@gmail.com>
2021-09-06 15:38:20 +0800

    version 0.1.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+import tmi from 'tmi.js'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const paramsId = new URLSearchParams(window.location.search).get("id");
+const paramsToken = new URLSearchParams(window.location.search).get("token");
+
+const client = new tmi.Client({
+  options: { debug: true, messagesLogLevel: "info" },
+  connection: {
+    reconnect: true,
+    secure: true
+  },
+  identity: {
+    username: (paramsId) ? paramsId : 'justinfan123456',
+    password: (paramsToken) ? paramsToken : ''
+  },
+  channels: ['zatd93']
+});
+
+class App extends Component {
+  tmiInit = () => {
+    client.connect().then((data) => {
+    }).catch((err) => {
+      console.log(err)
+    });
+
+    client.on("message", (target, context, msg, self) => {
+      
+    });
+  };
+
+  handleClick = (name) => {
+    client.say("zatd93", "!LP");
+  };
+
+  componentDidMount() {
+    this.tmiInit();
+}
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+        <Button 
+          variant='contained'
+          onClick={this.handleClick}
+        >LP Count</Button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
